37107511529f58c6d36485609880d5d99e4a378c
Hanno Gödecke <hgoedecke@cuvent.com>
2021-01-03 16:47:48 +0100

    Updated podfile, updated earth quake example

--- example/src/examples/EarthQuakes.js
+++ example/src/examples/EarthQuakes.js
@@ -3,8 +3,6 @@ import MapboxGL from '@react-native-mapbox-gl/maps';
 
 import sheet from '../styles/sheet';
 import {SF_OFFICE_COORDINATE} from '../utils';
-
-import BaseExamplePropTypes from './common/BaseExamplePropTypes';
 import Page from './common/Page';
 
 const layerStyles = {
@@ -44,51 +42,61 @@ const layerStyles = {
   },
 };
 
-class EarthQuakes extends React.Component {
-  static propTypes = {
-    ...BaseExamplePropTypes,
-  };
+const EarthQuakes = (props) => {
+  const sourceRef = React.useRef();
 
-  render() {
-    return (
-      <Page {...this.props}>
-        <MapboxGL.MapView
-          style={sheet.matchParent}
-          styleURL={MapboxGL.StyleURL.Dark}>
-          <MapboxGL.Camera
-            zoomLevel={6}
-            pitch={45}
-            centerCoordinate={SF_OFFICE_COORDINATE}
+  /**
+   * This is called when the user has moved the map and when the map has
+   * been fully rendered for the first time.
+   */
+  const regionChanged = React.useCallback(async () => {
+    const singleFeatures = await sourceRef.current.features([
+      '!',
+      ['has', 'point_count'],
+    ]);
+    console.log('Single features not clustered:', singleFeatures);
+  }, []);
+
+  return (
+    <Page {...props}>
+      <MapboxGL.MapView
+        style={sheet.matchParent}
+        onRegionDidChange={regionChanged}
+        styleURL={MapboxGL.StyleURL.Dark}>
+        <MapboxGL.Camera
+          zoomLevel={6}
+          pitch={45}
+          centerCoordinate={SF_OFFICE_COORDINATE}
+        />
+
+        <MapboxGL.ShapeSource
+          id="earthquakes"
+          ref={sourceRef}
+          cluster
+          clusterRadius={50}
+          clusterMaxZoom={14}
+          url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson">
+          <MapboxGL.SymbolLayer
+            id="pointCount"
+            style={layerStyles.clusterCount}
           />
 
-          <MapboxGL.ShapeSource
-            id="earthquakes"
-            cluster
-            clusterRadius={50}
-            clusterMaxZoom={14}
-            url="https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson">
-            <MapboxGL.SymbolLayer
-              id="pointCount"
-              style={layerStyles.clusterCount}
-            />
+          <MapboxGL.CircleLayer
+            id="clusteredPoints"
+            belowLayerID="pointCount"
+            filter={['has', 'point_count']}
+            style={layerStyles.clusteredPoints}
+          />
 
-            <MapboxGL.CircleLayer
-              id="clusteredPoints"
-              belowLayerID="pointCount"
-              filter={['has', 'point_count']}
-              style={layerStyles.clusteredPoints}
-            />
-
-            <MapboxGL.CircleLayer
-              id="singlePoint"
-              filter={['!', ['has', 'point_count']]}
-              style={layerStyles.singlePoint}
-            />
-          </MapboxGL.ShapeSource>
-        </MapboxGL.MapView>
-      </Page>
-    );
-  }
-}
+          <MapboxGL.CircleLayer
+            id="singlePoint"
+            filter={['!', ['has', 'point_count']]}
+            style={layerStyles.singlePoint}
+          />
+        </MapboxGL.ShapeSource>
+      </MapboxGL.MapView>
+    </Page>
+  );
+};
 
 export default EarthQuakes;
